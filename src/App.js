f18a89905809ad5a4becfda9928fd2e4500542f0
zhaosymoon <zhaosymoon@gmail.com>
2024-10-14 13:44:03 -0400

    to publish on github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Personal Website</h1>
+        <p>Hello! I’m Jay Zhao. This is my personal website where I showcase my projects and simple apps.</p>
       </header>
     </div>
   );
